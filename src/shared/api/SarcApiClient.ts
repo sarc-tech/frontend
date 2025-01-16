a42b08265614099fa70e2961ba273a84c1cfc6fb
Pavel Shorokhov <p-ex@ya.ru>
2025-01-16 03:24:07 +0300

    remove mocked login method

--- src/shared/api/SarcApiClient.ts
+++ src/shared/api/SarcApiClient.ts
@@ -1,15 +1,6 @@
 import { GeneratedSarcApiClient } from 'shared/api/generated';
-import { sleep } from 'shared/utils/promiseUtils';
 
 export class SarcApiClient extends GeneratedSarcApiClient {
-  async login(login: string, password: string): Promise<string> {
-    await sleep(1000);
-    if (login === 'ivan' && password === 'password1') {
-      return 'token-example-for-user-ivan';
-    }
-    throw new Error('Password wrong');
-  }
-
   setAuthToken(authToken: string | null) {
     this.request.config.TOKEN = authToken ?? undefined;
   }
